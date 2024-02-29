a7a0e0138f047db2d9cc162ef5686fcb53455fbf
Edon <edon@valdman.works>
2024-02-29 10:48:35 -0600

    Condensed `products` field, renamed primary target
    Condensed `products` field and renamed primary target `PokeAPIClient`.

--- Package.swift
+++ Package.swift
@@ -5,19 +5,16 @@ import PackageDescription
 
 let package = Package(
     name: "PokeAPI",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "PokeAPI",
-            targets: ["PokeAPI"]),
+    products: [.library(name: "PokeAPI", targets: ["PokeAPIClient"])],
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "PokeAPI"),
+            name: "PokeAPIClient"
+        ),
         .testTarget(
             name: "PokeAPITests",
-            dependencies: ["PokeAPI"]),
+            dependencies: ["PokeAPIClient"]),
     ]
 )
