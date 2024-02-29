6f984fc042bc546549f8304fe15e6a20a5d628ce
Edon <edon@valdman.works>
2024-02-29 10:48:54 -0600

    Update Package.swift
    Added specific platforms.

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "PokeAPI",
+    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .visionOS(.v1)],
     products: [.library(name: "PokeAPI", targets: ["PokeAPIClient"])],
     ],
     targets: [
